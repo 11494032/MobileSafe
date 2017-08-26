550d3ab244bb891010dfc281ea333af53d8fc7e6
11494032 <11494032@qq.com>
2017-08-26 15:02:42 +0800

    first commit

---
+++ app/src/main/java/com/moresmart/mobilesafe/act/splash.java
@@ -0,0 +1,15 @@
+package com.moresmart.mobilesafe.act;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.moresmart.mobilesafe.R;
+
+public class splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
